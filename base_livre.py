390710777b33f97a293cd695916c822fe74f649f
NJFresnay <115483949+NJFresnay@users.noreply.github.com>
2023-11-27 19:17:03 +0100

    Update base_livre.py
    Essaie de voir ce que j'ai modifié et mis en commetaire si cela ne peut pas nous aider a retourner les infos dont on a besoin

--- base_livre.py
+++ base_livre.py
@@ -1,28 +1,22 @@
-#!env/bin/ python3
-
-#classe base_livre
-#sous classes: PDF, EPUB
-
 import os
-from ebooklib import epub #pip install EbookLib #EbookLib 0.18
-from pypdf import PdfReader #pip install pypdf #pypdf 3.17.1
+from ebooklib import epub  #librairie pour traiter les documents de type "ebook"
+from pypdf import PdfReader #librairie pour traiter les documents de type "pdf"
 
 class base_livre:
     def __init__(self,ressource):
         self.ressource = ressource
-        if not os.path.exists(ressource): #si le path n'existe pas
-            raise FileNotFoundError(f"Ce file '{ressource}' n'existe pas.")
+        if not os.path.exists(ressource): #vérification de l'existance du path
+            raise FileNotFoundError(f"Ce fchier n'existe pas!")
 
     def type(self):
-        fichier_extension = os.path.splitext(self.ressource)[1].lower() #detecter l'extension du fichier: exemple: .pdf ou .epub
-        self.fichier_type = fichier_extension
-        
-        if self.fichier_type == ".pdf":
+        # fichier_extension = os.path.splitext(self.ressource)[1].lower() #détecte l'extension du fichier
+        # self.fichier_type = fichier_extension
+        if self.ressource.endswith(".pdf"):
             return PDF
-        elif self.fichier_type == ".epub":
+        elif self.ressource.endswith(".epub")":
             return EPUB
         else:
-            raise NotImplementedError("format indéfini")
+            raise ValueError("format non pris en charge")
 
     def titre(self):
         return self.type()(self.ressource).titre()
@@ -53,7 +47,7 @@ class PDF(base_livre):
         return livre.metadata.author
 
     def langue(self):
-        return None #pas de metadata pour la langue #selon la documentation de cette librarie python
+        raise AttributeError("Information non fournie selon la documentation")
 
     def sujet(self):
         livre = PdfReader(self.path)
@@ -62,6 +56,12 @@ class PDF(base_livre):
     def date(self):
         livre = PdfReader(self.path)
         return livre.metadata.creation_date
+
+    def __repr__(self):
+        return self.titre(), self.auteur(), self.Type(), self.date(), self.sujet()
+
+    def __str__(self):
+        return f"Titre: {self.titre()}, \nAuteur(s): {self.auteur()},\nType de fichier: {self.Type()}, \nDate de publication: {self.date()}"
 
 class EPUB(base_livre):
     
@@ -81,9 +81,33 @@ class EPUB(base_livre):
         return livre.get_metadata("DC","language")
     
     def sujet(self):
-        return None #selon le documentation y a pas de metadata pour le sujet
+        raise AttributeError("Information non fournie selon la documentation")
 
     def date(self):
         livre = epub.read_epub(self.path)
         return livre.get_metadata("DC","date")
+
+# def extract_metadata_epub(epub_path):
+#     book = epub.read_epub(epub_path)
+#     metadata = {
+#         'title': book.get_metadata('DC', 'title')[0][0] if book.get_metadata('DC', 'title') else None,
+#         'author': ', '.join(author[0] for author in book.get_metadata('DC', 'creator')) if book.get_metadata('DC', 'creator') else None,
+#         'language': book.get_metadata('DC', 'language')[0][0] if book.get_metadata('DC', 'language') else None,
+#         'identifier': book.get_metadata('DC', 'identifier')[0][0] if book.get_metadata('DC', 'identifier') else None,
+#         'subject': book.get_metadata('DC', 'subject')[0][0] if book.get_metadata('DC', 'subject') else None,
+#         'date': book.get_metadata('DC', 'date')[0][0] if book.get_metadata('DC', 'date') else None,
+#         'publisher': book.get_metadata('DC', 'publisher')[0][0] if book.get_metadata('DC', 'publisher') else None,
+#         'description': book.get_metadata('DC', 'description')[0][0] if book.get_metadata('DC', 'description') else None,
+#         # Ajoutez d'autres métadonnées selon vos besoins
+#     }
+
+
+
+
+
+    # def __repr__(self):
+    #     return self.titre(), self.auteur(),self.langue(), self.Type(), self.date(), self.sujet()
+
+    # def __str__(self):
+    #     return f"Titre: {self.titre()}, \nAuteur(s): {self.auteur()}, \nlangue: {self.langue()},\nType de fichier: {self.Type()}, \nDate de publication: {self.date}, \nCatégorie: {self.sujet()}"
 
