34f70c5e6371424ad2dbe288af747c7e03a4567b
rayanejaffal <rayanejaffal@Rayane-PC>
2023-12-01 10:22:08 +0100

    ce class bien passe par les liens dans une pafe html mais besoin des modifications

---
+++ bibli-scrap.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+
+class bibli_scrap:
+    
+    def __init__(self):
+        pass
+
+    def scrap(self, url, profondeur, nbmax):
+        
+        if profondeur == 0 or nbmax == 0:
+            return
+
+        html_page = requests.get(url, verify=False).content
+        soup = BeautifulSoup(html_page, "html.parser")
+        livres_urls = []
+    
+        for l in soup.find_all("a",):
+            lien = l.get('href', [])
+            if lien.endswith('.pdf') or lien.endswith('.epub'):
+                try:
+                   ## lien = "https://" + lien
+                    reponse = requests.get(lien)
+                    with open(l.title, mode="wb") as file:
+                        file.write(reponse.content)
+                    livres_urls.append(lien)
+                    self.nbmax -= 1
+                    
+                except NotImplementedError as e:
+                    print(f"Error downloading {lien}: {e}")
+
+                except Exception as e:
+                    print(f"An unexpected error occurred: {e}")
+
