32c5150a331264e6d137c91ba47e5a1ae0d2be5c
rayanejaffal <rayanejaffal@Rayane-PC>
2023-12-02 12:32:41 +0100

    class capable de télecharger les fichier d'une page web, le profondeur doit initialiser à 1 pour le moment

--- bibli_scrap.py
+++ bibli_scrap.py
@@ -1,40 +1,52 @@
 from bs4 import BeautifulSoup
 import requests
+import os
 
 class bibli_scrap:
+    """
+    exemple d'utilisation:
     
-    def __init__(self):
-        pass
+    path = bibli_scrap(r"C:\Users\jaffa\OneDrive\Desktop\Bibliotheque")
 
-    def scrap(url, profondeur, nbmax):
+    path.scrap("https://math.univ-angers.fr/~jaclin/biblio/livres/", 1, 2)
+    """
+
+    def __init__(self,bibli_path):
+        self.bibli_path = bibli_path ##juste modifiez cette fonction d'une facon qu'elle hérite le path du folder de la bibliotheque de l'autre class
         
-        if profondeur == 0 or nbmax == 0:
+    def scrap(self, url, profondeur, nbmax):
+        i = 0 #initialiser le compteur
+        if profondeur == 0 or nbmax == 0: #si les arguments sont zero on sortie
             return
         
-        directory = os.path.join(os.getcwd(), "Desktop", "Bibliotheque")
-
-        if not os.path.exists(directory):
+        directory = self.bibli_path #je détermine le directoire
+        
+        if not os.path.exists(directory): #si le directoire n'existe pas on la créer
             os.makedirs(directory)
-
-        html_page = requests.get(url, verify=False).content
-        soup = BeautifulSoup(html_page, "html.parser")
-        i = 0
-    
+        
         try:
-            for i, l in enumerate(soup.find_all("a")):
-                lien = l.get('href', [])
-                if lien.endswith('.pdf') or lien.endswith('.epub'):
+            html_page = requests.get(url, verify=False).content
+            soup = BeautifulSoup(html_page, "html.parser")
+
+            for l in soup.find_all("a"): #ici on cherche les lien
+                lien = l.get('href', []) #on extract les liens
+                if lien.endswith('.pdf') or lien.endswith('.epub'): 
                     try:
-                        lien = "https://math.univ-angers.fr/~jaclin/biblio/livres/" + lien
-                        reponse = requests.get(lien)
-                        filename = os.path.join(directory, os.path.basename(lien))
-                        with open(l.title, mode="wb") as file:
+                        if 'https://' not in lien:
+                            lien = url + lien #ajouter le nom du server au lien incomplet
+                        reponse = requests.get(lien, verify =False)
+                        
+                        filename = os.path.join(directory, os.path.basename(lien)) #nommer le fichier de chaque livre selon le nom du base du lien
+                        
+                        with open(filename, mode="wb") as file: #télechargement
                             file.write(reponse.content)
-                        self.nbmax -= 1
-                    
+                        i +=1
+                        if i >= nbmax: #on a dépasser le nombre maw des fichier à télécharger
+                            break
+                        
                     except requests.exceptions.RequestException as e:
                         print(f"Erreur du téléchargement {lien}: {e}")
                         
         except requests.exceptions.RequestException as e:
             print(f"Un erreur inattendu: {e}")
-
+            
