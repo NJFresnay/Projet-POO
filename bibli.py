5916797387b33a86862a9b4f5626d067ef30a2b7
NJFresnay <115483949+NJFresnay@users.noreply.github.com>
2023-12-03 11:10:50 +0100

    Update bibli.py

--- bibli.py
+++ bibli.py
@@ -1,4 +1,5 @@
 import base_bibli, bibli_scrap
+import os
 
 # La classe bibli sert à alimenter notre bibliothèque à l'aide du web scraping.
 # Pour se faire, elle hérite des méthodes de "base_bibli" pour la génération des rapports
@@ -12,7 +13,8 @@ class bibli(base_bibli, bibli_scrap):
         base_bibli.__init__(path)
         bibli_scrap.__init__(path)
 
-    def alimenter(self, url, profondeur=1, nbmax):
+    def alimenter(self, url, profondeur, nbmax):
+        profondeur = 1
         if os.path.exists(url): #si c'est un fichier sur notre machine appelle 'ajouter()'
             return self.ajouter(url)
         else:  #sinon c'est une url, appelle 'scrap()'
