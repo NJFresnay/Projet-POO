c52c44d7333e9b52e12de6261fc48b085ae1b6e0
rayanejaffal <130594483+rayanejaffal@users.noreply.github.com>
2023-12-03 18:34:46 +0100

    Done

--- bibli.py
+++ bibli.py
@@ -1,21 +1,23 @@
-import base_bibli, bibli_scrap
+from base_bibli import base_bibli
 import os
 
 # La classe bibli sert à alimenter notre bibliothèque à l'aide du web scraping.
 # Pour se faire, elle hérite des méthodes de "base_bibli" pour la génération des rapports
 # et de "bibli_scrap" pour récupérer les documents depuis l'url directement 
 
-class bibli(base_bibli, bibli_scrap):
+class bibli(base_bibli):
     
     def __init__(self, path):
         """ Vous devez lui passer en arguments le chemin vers le répertoire
             qui vous servira de bibliothèque"""
-        base_bibli.__init__(path)
-        bibli_scrap.__init__(path)
+        super().__init__(path)
+        from bibli_scrap import bibli_scrap  
+        self.scrap_instance = bibli_scrap(path)
 
-    def alimenter(self, url, profondeur, nbmax):
-        profondeur = 1
+    def alimenter(self, url, nbmax):
         if os.path.exists(url): #si c'est un fichier sur notre machine appelle 'ajouter()'
             return self.ajouter(url)
         else:  #sinon c'est une url, appelle 'scrap()'
-            return self.scrap(url, profondeur, nbmax)        
+            #selon le sujet elle ajoute tous les livres référencés dans la page web correspondant à l’URL
+            profondeur = 1 #alors prof = 1
+            return self.scrap_instance.scrap(url, profondeur, nbmax)        
